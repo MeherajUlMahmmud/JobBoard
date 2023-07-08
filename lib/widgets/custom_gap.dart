1cbdd0a63ec717110a4dc5cf7c1329320964870d
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2023-07-08 19:26:11 +0600

    Create custom_gap.dart

---
+++ lib/widgets/custom_gap.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GapWidget extends StatelessWidget {
+  const GapWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 20,
+      // margin: const EdgeInsets.only(top: 10, bottom: 10),
+      color: Colors.grey.shade200,
+    );
+  }
+}
