29e66fc3b07dca7b2e5095ac80eee13e611a8c0d
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2023-07-08 19:26:44 +0600

    Update SplashScreen.dart

--- lib/screens/utility_screens/SplashScreen.dart
+++ lib/screens/utility_screens/SplashScreen.dart
@@ -1,7 +1,9 @@
+import 'package:jobboard/providers/user_provider.dart';
 import 'package:jobboard/screens/auth_screens/LoginScreen.dart';
 import 'package:jobboard/screens/home_screen.dart';
 import 'package:jobboard/utils/local_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SplashScreen extends StatefulWidget {
   static const routeName = "/";
@@ -16,11 +18,14 @@ class _SplashScreenState extends State<SplashScreen> {
 
   void checkUser() async {
     final Map<String, dynamic> tokens = await localStorage.readData('tokens');
+    final Map<String, dynamic> user = await localStorage.readData('user');
     if (tokens['access'] == null || tokens['refresh'] == null) {
       Future.delayed(const Duration(seconds: 2), () {
         Navigator.pushReplacementNamed(context, LoginScreen.routeName);
       });
     } else {
+      Provider.of<UserProvider>(context, listen: false).setUser(user);
+      Provider.of<UserProvider>(context, listen: false).setTokens(tokens);
       Future.delayed(const Duration(seconds: 2), () {
         Navigator.pushReplacementNamed(context, HomeScreen.routeName);
       });
